14290df03a2d9a825237735f8431f2c9f7104a50
EuiJun Jeong <euijj@amazon.com>
2021-11-22 15:39:07 +0900

    update pascal triangle 2

--- leetcode/pascal-triangle/main.go
+++ leetcode/pascal-triangle/main.go
@@ -18,7 +18,9 @@ Constraints:
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func generateTriangle(depth int, prev []int, triangle [][]int) [][]int {
 	triangle = append(triangle, prev)
@@ -48,9 +50,68 @@ func generate(numRows int) [][]int {
 	return generateTriangle(numRows, seed, triangle)
 }
 
+/*
+https://leetcode.com/problems/pascals-triangle-ii/
+Pascal's Triangle II
+Given an integer `rowIndex`, return the `rowIndex`th (**0-indexed**) row of the **Pascal's triangle**.
+In **Pascal's triangle**, each number is the sum of the two numbers directly above it as shown:
+Example 1:
+	Input: rowIndex = 3
+	Output: [1,3,3,1]
+
+Example 2:
+	Input: rowIndex = 0
+	Output: [1]
+
+Example 3:
+	Input: rowIndex = 1
+	Output: [1,1]
+ */
+func getRow(rowIndex int) []int {
+	return getLastRowTriangle(0, rowIndex + 1, []int{})
+}
+
+func getLastRowTriangle(depth, size int, prev []int) []int{
+
+	if depth == size {
+		return prev
+	}
+	var curr []int
+	for i:=0; i<=depth; i++ {
+		if i == 0 || i == depth {
+			curr = append(curr, 1)
+		} else {
+			curr = append(curr, prev[i-1] + prev[i])
+		}
+	}
+	return getLastRowTriangle(depth+1, size, curr)
+}
+
+func recursiveTriangle(depth, size int, output [][]int) [][]int{
+
+	if depth == size {
+		return output
+	}
+	var row []int
+	for i:=0; i<=depth; i++ {
+		if i == 0 || i == depth {
+			row = append(row, 1)
+		} else {
+			prevRow := output[len(output)-1]
+			row = append(row, prevRow[i-1] + prevRow[i])
+		}
+	}
+	output = append(output, row)
+	return recursiveTriangle(depth+1, size, output)
+}
+
 func main() {
 
 	fmt.Println(generate(5))
 	fmt.Println(generate(1))
 
+	fmt.Println(getRow(3))
+
+	ret := recursiveTriangle(0, 5, [][]int{})
+	fmt.Println(ret)
 }
