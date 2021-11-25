7c12d67689f15e11df2611e939dcad0baf7796b1
EuiJun Jeong <euijj@amazon.com>
2021-11-25 18:32:34 +0900

    upload implementation trie

--- leetcode/house-robber/main.go
+++ leetcode/house-robber/main.go
@@ -27,55 +27,53 @@ import (
 )
 
 //dynamic programming
-func rob(nums []int) int {
-	N := len(nums)
-
-	if N == 0 {
-		return 0
-	}
-
-	maxRobbedAmount := make([]int, len(nums)+1)
-	maxRobbedAmount[N] = 0
-	maxRobbedAmount[N-1] = nums[N-1]
-
-	for i := N - 2; i >= 0; i-- {
-		maxRobbedAmount[i] = int(math.Max(float64(maxRobbedAmount[i + 1]), float64(maxRobbedAmount[i + 2] + nums[i])))
-	}
-	return maxRobbedAmount[0]
-}
-
-//recursive + memoization
 //func rob(nums []int) int {
-//	memo := make([]int, len(nums))
+//	N := len(nums)
 //
-//	for i:=0; i<len(memo); i++ {
-//		memo[i] = -1
-//	}
-//
-//	return robFrom(0, nums, memo)
-//}
-//
-//func robFrom(i int, nums []int, memo []int) int {
-//	if i >= len(nums) {
+//	if N == 0 {
 //		return 0
 //	}
 //
-//	if memo[i] > -1 {
-//		return memo[i]
-//	}
+//	maxRobbedAmount := make([]int, len(nums)+1)
+//	maxRobbedAmount[N] = 0
+//	maxRobbedAmount[N-1] = nums[N-1]
 //
-//	ans := int(math.Max(float64(robFrom(i+1, nums, memo)), float64(robFrom(i+2, nums, memo) + nums[i])))
-//	memo[i] = ans
-//	return ans
+//	for i := N - 2; i >= 0; i-- {
+//		maxRobbedAmount[i] = int(math.Max(float64(maxRobbedAmount[i + 1]), float64(maxRobbedAmount[i + 2] + nums[i])))
+//	}
+//	return maxRobbedAmount[0]
 //}
 
-func main() {
-	num := []int{114, 117, 207, 117, 235, 82, 90, 67, 143,
-		146, 53, 108, 200, 91, 80, 223, 58, 170, 110, 236,
-		81, 90, 222, 160, 165, 195, 187, 199, 114, 235, 197,
-		187, 69, 129, 64, 214, 228, 78, 188, 67, 205, 94, 205,
-		169, 241, 202, 144, 240}
+//recursive + memoization
+func rob(nums []int) int {
+	memo := make([]int, len(nums))
+	for i:=0; i<len(memo); i++ {
+		memo[i] = -1
+	}
+	return robFrom(0, nums, memo)
+}
 
-	//num := []int{1,2,1,1}
+func robFrom(i int, nums []int, memo []int) int {
+	if i >= len(nums) {
+		return 0
+	}
+	if memo[i] > -1 {
+		return memo[i]
+	}
+
+	ans := int(math.Max(float64(robFrom(i+1, nums, memo)), float64(robFrom(i+2, nums, memo) + nums[i])))
+	memo[i] = ans
+	fmt.Println(ans)
+	return ans
+}
+
+func main() {
+	//num := []int{114, 117, 207, 117, 235, 82, 90, 67, 143,
+	//	146, 53, 108, 200, 91, 80, 223, 58, 170, 110, 236,
+	//	81, 90, 222, 160, 165, 195, 187, 199, 114, 235, 197,
+	//	187, 69, 129, 64, 214, 228, 78, 188, 67, 205, 94, 205,
+	//	169, 241, 202, 144, 240}
+
+	num := []int{2,7,9,3,1}
 	fmt.Println(rob(num))
 }
