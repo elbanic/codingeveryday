576238a8e4bd30ba63eda15e8194883f7465d376
EuiJun Jeong <euijj@amazon.com>
2022-01-30 14:53:35 +0900

    update rotate array

--- leetcode/rotate-array/main.go
+++ leetcode/rotate-array/main.go
@@ -2,6 +2,45 @@ package main
 
 import "fmt"
 
+//reverse
+func rotate3(nums []int, k int) {
+	k = k % len(nums)
+	reverse(nums)
+	reverse(nums[:k])
+	reverse(nums[k:])
+}
+
+func reverse(nums []int) {
+	for i := 0; i < len(nums)/2; i++ {
+		temp := nums[len(nums)-1-i]
+		nums[len(nums)-1-i] = nums[i]
+		nums[i] = temp
+	}
+}
+
+//Cyclic Replacements
+func rotate2(nums []int, k int) {
+	k = k % len(nums)
+	var count int
+
+	for start := 0; count < len(nums); start++ {
+		current := start
+		prev := nums[start]
+
+		for {
+			next := (current + k) % len(nums)
+			temp := nums[next]
+			nums[next] = prev
+			prev = temp
+			current = next
+			count++
+			if start == current {
+				break
+			}
+		}
+	}
+}
+
 func rotate(nums []int, k int) {
 
 	if len(nums) <= k {
@@ -20,9 +59,9 @@ func rotate(nums []int, k int) {
 }
 
 func main() {
-	nums := []int{1, 2, 3}
-	k := 5
+	nums := []int{1, 2, 3, 4, 5}
+	k := 2
 
-	rotate(nums, k)
+	rotate3(nums, k)
 	fmt.Println(nums)
 }
