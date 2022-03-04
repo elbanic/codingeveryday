598d7fe767f24c9b4c565f8ae28cc4cf1338b0fe
EuiJun Jeong <euijj@amazon.com>
2022-03-05 00:09:56 +0900

    upload champagne tower

--- leetcode/champagne-tower/main.go
+++ leetcode/champagne-tower/main.go
@@ -1,17 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func champagneTower(poured int, query_row int, query_glass int) float64 {
-
-	var glass [][]int
-
-	for i := 0; i < poured; i++ {
-
+	glass := make([][]float64, 102)
+	for i := range glass {
+		glass[i] = make([]float64, i+1)
 	}
+	glass[0][0] = float64(poured)
+	for r := 0; r <= query_row; r++ {
+		for c := 0; c <= r; c++ {
+			q := (glass[r][c] - 1) / 2
+			if q > 0 {
+				glass[r+1][c] += q
+				glass[r+1][c+1] += q
+			}
+		}
+	}
+	return math.Min(1., glass[query_row][query_glass])
 }
 
 func main() {
-	poured := 2
-	query_row := 1
-	query_glass := 1
-	champagneTower(poured, query_row, query_glass)
+	fmt.Println(champagneTower(7, 3, 1))
 }
