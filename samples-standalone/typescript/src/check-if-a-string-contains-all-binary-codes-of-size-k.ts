b6272bab0ef11a1d926b7c8ee7d6c80f974e1a33
EuiJun Jeong <euijj@amazon.com>
2022-05-31 21:39:49 +0900

    upload parallel courses

--- samples-standalone/typescript/src/check-if-a-string-contains-all-binary-codes-of-size-k.ts
+++ samples-standalone/typescript/src/check-if-a-string-contains-all-binary-codes-of-size-k.ts
@@ -2,11 +2,11 @@
 namespace check_if_a_string_contains_all_binary_codes_of_size_k {
 
     function hasAllCodes2(s: string, k: number): boolean {
-        let need = 1 << k
-        const got : Set<string> = new Set<string>()
+        let need = Math.pow(2, k)
+        const got: Set<string> = new Set<string>()
 
-        for (let i=k; i<=s.length; i++) {
-            let a = s.slice(i-k, i)
+        for (let i = k; i <= s.length; i++) {
+            let a = s.slice(i - k, i)
             if (!got.has(a)) {
                 got.add(a)
                 need--
@@ -27,9 +27,9 @@ namespace check_if_a_string_contains_all_binary_codes_of_size_k {
 
         const codes = getCodes(k)
         const subs: string[] = new Array()
-        for (let i=0; i<=s.length-k; i++) {
-            if (codes.has(s.slice(i, i+k))){
-                codes.set(s.slice(i, i+k), 0)
+        for (let i = 0; i <= s.length - k; i++) {
+            if (codes.has(s.slice(i, i + k))) {
+                codes.set(s.slice(i, i + k), 0)
             }
         }
 
@@ -48,7 +48,7 @@ namespace check_if_a_string_contains_all_binary_codes_of_size_k {
         }
         return codes
     }
-    
-    const s = "1011110111001001101001110001100111101111010101011100111001110010010001000111010110101110000110101001011100100010100110011101011110001000100010101101011", k = 20
-    console.log(hasAllCodes(s, k))
+
+    const s = "00110110", k = 2
+    console.log(hasAllCodes2(s, k))
 }
--- samples-standalone/typescript/src/parallel-courses.ts
+++ samples-standalone/typescript/src/parallel-courses.ts
@@ -1,10 +1,69 @@
+
 namespace parallel_courses {
 
     function minimumSemesters(n: number, relations: number[][]): number {
 
-        return 0
+        if (relations.length == 0) {
+            return -1
+        }
+
+        relations.sort((a, b) => b[1] - a[1])
+        const lastCourse = relations[0][1]
+
+        const visited = new Map<number, number>()
+        for (let i = 0; i <= n; i++) {
+            visited.set(i, 0)
+        }
+
+        for (let node = 0; node <= n; node++) {
+            if (isCycle(relations, node, visited) == -1) {
+                return -1
+            }
+        }
+
+        visited.forEach((value, key) => visited.set(key, 0))
+        let maxLength = 1
+        for (let node = 0; node <= n; node++) {
+            const length = maxPathDFS(relations, node, visited);
+            maxLength = Math.max(length, maxLength);
+        }
+        return maxLength
     };
 
-    const n = 3, relations = [[1, 3], [2, 3], [3, 4]]
+    function maxPathDFS(relations: number[][], node: number, visited: Map<number, number>): number {
+
+        if (visited.get(node) != 0) {
+            return visited.get(node)
+        }
+
+        let maxLength = 1
+        const children = relations.filter(x => x[1] == node).map(x => x[0])
+        for (let child of children) {
+            const length = maxPathDFS(relations, child, visited)
+            maxLength = Math.max(length + 1, maxLength)
+        }
+        visited.set(node, maxLength)
+        return maxLength
+    }
+
+    function isCycle(relations: number[][], node: number, visited: Map<number, number>): number {
+
+        if (visited.get(node) != 0) {
+            return visited.get(node)
+        } else {
+            visited.set(node, -1)
+        }
+
+        const children = relations.filter(x => x[1] == node).map(x => x[0])
+        for (let child of children) {
+            if (isCycle(relations, child, visited) == -1) {
+                return -1
+            }
+        }
+        visited.set(node, 1)
+        return 1
+    }
+
+    const n = 3, relations = [[1, 3], [2, 3]]
     console.log(minimumSemesters(n, relations))
 }
