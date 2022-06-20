81c3e2e52664d8b6f82a9e1038b4deb68416fa51
elbanic <elbanic12@naver.com>
2022-06-19 17:46:00 -0700

    upload isiomorphic strings

---
+++ samples-standalone/typescript/src/isomorphic-strings.ts
@@ -0,0 +1,30 @@
+namespace isomorphic_strings {
+    function isIsomorphic(s: string, t: string): boolean {
+        if (s.length != t.length) {
+            return false
+        }
+        return isIsomorphicHelper(s, t) && isIsomorphicHelper(t, s)
+    };
+
+    function isIsomorphicHelper(s: string, t: string): boolean {
+        let seen = new Set<string>()
+        let map = new Map<string, string>()
+        for (let i = 0; i < s.length; i++) {
+            if (!seen.has(s[i])) {
+                seen.add(s[i])
+                map.set(s[i], t[i])
+            } else {
+                if (map.get(s[i]) != t[i]) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    console.log(isIsomorphic("egg", "add"))
+    console.log(isIsomorphic("bbbaaaba", "aaabbbba"))
+    console.log(isIsomorphic("badc", "baba"))
+    console.log(isIsomorphic("paper", "title"))
+
+}
